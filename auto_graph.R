e0320e0d675fba84394b0cedad719fa66f2c7888
sheffre <107573605+sheffre@users.noreply.github.com>
2022-10-26 18:50:16 +0300

    Update auto_graph.R
    added axis autoscaling

--- auto_graph.R
+++ auto_graph.R
@@ -1,17 +1,30 @@
+library(ggplot2)
+
 reg_3_data <- read.csv2(file = "C:/The_Kara_Sea_project/data/region_3.csv")
+
 
 reg_3_data$factor_horizon <- as.factor(reg_3_data$Depth)
 
 for(x in levels(reg_3_data$factor_horizon)) {
-  ggplot(reg_3_data[reg_3_data$factor_horizon == x, ], aes(Year, Omega_Ar)) +
+  ggplot(reg_3_data[reg_3_data$factor_horizon == x, ], aes(Year, pH)) +
     geom_line(color = "blue", size = 0.1) +
     geom_point(color = "red") +
     geom_smooth(method = lm, se = F, col = "green", size = 0.5) +
-    scale_x_continuous(breaks = seq(1936, 2022, 4)) +
-    scale_y_continuous(breaks = seq(1, 14, 0.05))
+    scale_x_continuous(breaks = seq(min(reg_3_data$Year, na.rm = T), 
+                                    max(reg_3_data$Year, na.rm = T),
+                                    (max(reg_3_data$Year, na.rm = T) -
+                                      min(reg_3_data$Year, na.rm = T))%%10)) +
+    scale_y_continuous(breaks = seq(round(min(reg_3_data$pH, na.rm = T), 
+                                          digits = 2),
+                                    round(max(reg_3_data$pH, na.rm = T),
+                                          digits = 2),
+                                    round(0.1*(max(reg_3_data$pH, na.rm = T) - 
+                                                 min(reg_3_data$pH, na.rm = T)), 
+                                          digits = 2)))
   
-  ggsave(filename =  paste("C:/The_Kara_Sea_project/output/graph/omega_Ar/horizon",
+  ggsave(filename =  paste("C:/The_Kara_Sea_project/output/graph/pH/horizon",
                            paste(x), "jpeg", sep = "."), 
          device = "jpeg", width = 2500, height = 1406, 
          units = "px", dpi = 300)
 }
+
